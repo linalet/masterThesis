442ea29f47e684050ba8b31e849117e898181b55
Nina Letenayova <ninalet@mail.muni.cz>
2026-03-04 20:22:49 -0700

    More data (2000)

--- coding/game_color_analysis/src/main.py
+++ coding/game_color_analysis/src/main.py
@@ -11,7 +11,7 @@ from igdb_api import download_image, query_igdb
 
 
 # Analysis settings
-START_YEAR = 1988
+START_YEAR = 2001
 # 1950  # Tennis for two 1958? OXO 1952?
 END_YEAR = 2025
 MAX_SCREENSHOTS_PER_GAME = 5  # possibly increase to 10
